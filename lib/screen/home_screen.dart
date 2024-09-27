f23f3c57229541a840d1f200505f04845b945866
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-27 12:42:01 +0300

    ADD Pay Package

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pay/pay.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      // backgroundColor: Colors.red,
       body: Center(
-        child: Text('Hello World!'),
+        child: Text('Hello World!',
+            style: TextStyle(fontSize: 24, color: Colors.red)),
       ),
     );
   }
